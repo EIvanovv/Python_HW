a6cc6d75128975f616bf38543230d6c3294f5bd1
EIvanovv <e.ivanovv00@gmail.com>
2025-07-01 20:57:40 +0300

    missed a $ sign in task 3

--- logical_expressions_and_conditional_statements_tasks.py
+++ logical_expressions_and_conditional_statements_tasks.py
@@ -8,14 +8,14 @@
 
 ### Your code here
 
-number = float(input("Enter a number: "))
+# number = float(input("Enter a number: "))
 
-if number==0:
-    print("Zero")
-elif number>0:
-    print("Positive")
-else:
-    print("Negative")
+# if number==0:
+#     print("Zero")
+# elif number>0:
+#     print("Positive")
+# else:
+#     print("Negative")
 
 
 ### EXPECTED OUTPUT:
